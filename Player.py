d00a870b5de6e25e2110005f0f339d54e687d360
Fuyuyukii <vinicius.aj.borges@gmail.com>
2023-02-08 09:39:52 -0300

    na escola >///<

--- Player.py
+++ Player.py
@@ -1,22 +1,30 @@
-# from Database import players
+from Database import players
 
 
 class player:
     class_identifier = "PL"
     current_letter = 0  # show in which generation the object are in.
     id_number = 0
-    # objects = players
+    objects = players
 
-    def __init__(self, id):
+    def __init__(self, id, name:str=None):
         self.__id = id
+        self.__name = name
 
+    # Properties
 
-players = {}
+    @property
+    def id(self):
+        return self.__id
+    
+    @id.setter
+    def id(self, value):
+        self.__id = value
 
-def create_object(class_in_use, id):
-    if id not in players:
-        players[id] = class_in_use(id)
-    return players[id]
-
-
-create_object(player, "A1")
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, value):
+        self.__name = value 
